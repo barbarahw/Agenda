39e9fa27b4c382f24b259066c79effb0703cdd96
barbarahw <127110674+barbarahw@users.noreply.github.com>
2024-06-23 12:34:12 -0300

    Add files via upload

---
+++ ProjetoFinal/src/dao/UsuarioDAO.java
@@ -0,0 +1,18 @@
+package dao;
+
+import java.sql.Connection;
+
+import entities.Usuario;
+
+public class UsuarioDAO {
+
+	private Connection conn;
+	
+	public UsuarioDAO(Connection conn) {
+		this.conn = conn;
+	}
+	
+	public void cadastrar(Usuario usuario) {
+		//TODO
+	}
+}
