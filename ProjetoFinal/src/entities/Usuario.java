40ffe27498e5ac316a0222340285fcaa3327b044
barbarahw <127110674+barbarahw@users.noreply.github.com>
2024-06-23 12:36:42 -0300

    Update Usuario.java

--- ProjetoFinal/src/entities/Usuario.java
+++ ProjetoFinal/src/entities/Usuario.java
@@ -12,4 +12,78 @@ public class Usuario {
 	private Date dataNascimento;
 	
 	
+	public Usuario(String usuario, String senha, String nome, String sexo, String email, Date dataNascimento) {
+		this.usuario = usuario;
+		this.senha = senha;
+		this.nome = nome;
+		this.sexo = sexo;
+		this.email = email;
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public Usuario() {
+		
+	}
+
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+
+	public String getSenha() {
+		return senha;
+	}
+
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getSexo() {
+		return sexo;
+	}
+
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	
 }
